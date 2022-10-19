2ffa32b2e166aa0db1e3b67e4d30ee2960bf9b10
S.Haturusinghe <haturusingh3@gmail.com>
2022-10-19 14:25:27 +0530

    first commit

---
+++ src/app/scorer-view/scorer-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'crx-scorer-view',
+  templateUrl: './scorer-view.component.html',
+  styleUrls: ['./scorer-view.component.scss']
+})
+export class ScorerViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
