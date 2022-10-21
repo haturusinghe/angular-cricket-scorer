97ec38f480768be0930eb24fe58f6d838f7d9d23
S.Haturusinghe <haturusingh3@gmail.com>
2022-10-21 19:47:00 +0530

    read from form values

--- src/app/scorer-view/runs-panel/runs-panel.component.ts
+++ src/app/scorer-view/runs-panel/runs-panel.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
 @Component({
   selector: 'crx-runs-panel',
   templateUrl: './runs-panel.component.html',
@@ -7,12 +8,13 @@ import { FormBuilder } from '@angular/forms';
 })
 export class RunsPanelComponent implements OnInit {
   run: number = 0;
-  toppings = this._formBuilder.group({
+  extras = this._formBuilder.group({
     WD: false,
     NB: false,
     B: false,
     LB: false,
   });
+  isWicket: boolean = false;
 
   wicket: Wicket[] = [
     { value: 'B', viewValue: 'Bowled' },
@@ -23,12 +25,26 @@ export class RunsPanelComponent implements OnInit {
     { value: 'HW', viewValue: 'Hit-wicket' },
   ];
 
+  wicketControl = new FormControl(this.wicket[0].value);
+
   constructor(private _formBuilder: FormBuilder) {}
 
   ngOnInit(): void {}
 
-  changeRun(run: number) {
-    this.run = run;
+  changeRun(run: string) {
+    this.run = parseInt(run);
+  }
+
+  onClick() {
+    console.log(this.run);
+    console.log(this.extras.value);
+    if (this.isWicket) {
+      console.log(this.wicketControl.value);
+    }
+  }
+
+  setWicketStatus() {
+    this.isWicket = !this.isWicket;
   }
 }
 
