deaadd99ef8eba69d2ef340b06b6f29958d40d50
S.Haturusinghe <haturusingh3@gmail.com>
2022-10-22 13:54:58 +0530

    get&display bowlerdata from service & changebowler

--- src/app/scorer-view/bowler-panel/bowler-panel.component.ts
+++ src/app/scorer-view/bowler-panel/bowler-panel.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { BowlerScore } from '../i/bowler-score';
+import { MatchDataServiceService } from '../services/match-data-service.service';
 
 @Component({
   selector: 'crx-bowler-panel',
@@ -6,24 +8,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bowler-panel.component.scss'],
 })
 export class BowlerPanelComponent implements OnInit {
-  displayedColumns: string[] = ['name', 'o', 'm', 'r', 'w', 'er'];
+  displayColumns: string[] = [
+    'player',
+    'runs',
+    'maidenOvers',
+    'overs',
+    'wickets',
+    'economyRate',
+  ];
 
-  dataSource = BALL_DATA;
+  bowler: BowlerScore = {
+    player: { id: 99, first_name: 'Axl', last_name: 'Rose' },
+    runs: -1,
+    maidenOvers: -1,
+    overs: -1,
+    wickets: -1,
+    economyRate: -1,
+  };
 
-  constructor() {}
+  bowlerTableData = [this.bowler];
 
-  ngOnInit(): void {}
+  constructor(private matchDataService: MatchDataServiceService) {}
+
+  ngOnInit(): void {
+    this.updatePlayers();
+  }
+
+  getBowler(): void {
+    this.matchDataService
+      .getBowlerDetails()
+      .subscribe((b) => (this.bowler = b));
+  }
+
+  updatePlayers() {
+    this.getBowler();
+    this.bowlerTableData = [this.bowler];
+  }
+
+  changeBowler() {
+    this.matchDataService.changeBowler();
+    this.updatePlayers();
+  }
 }
 
-export interface BallerTableRow {
-  name: string;
-  o: number;
-  m: number;
-  r: number;
-  w: number;
-  er: number;
-}
 
-const BALL_DATA: BallerTableRow[] = [
-  { name: 'Jane Doe*', o: 4, m: 3, r: 1, w: 0, er: 15.5 },
-];
