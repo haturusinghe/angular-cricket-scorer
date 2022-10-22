f7b39b51c16181f7308282493604845382265645
S.Haturusinghe <haturusingh3@gmail.com>
2022-10-22 12:26:00 +0530

    temp player and team data + interfaces

---
+++ src/app/data/player-data.ts
@@ -0,0 +1,37 @@
+import { Team } from '../scorer-view/i/team';
+
+export const TEAMS: Team[] = [
+  {
+    teamName: 'Sri Lanka',
+    players: [
+      { id: 1, first_name: 'Petr', last_name: 'Blose' },
+      { id: 2, first_name: 'Dorolice', last_name: 'Dossetter' },
+      { id: 3, first_name: 'Jonis', last_name: 'Baughn' },
+      { id: 4, first_name: 'Alissa', last_name: 'Peyzer' },
+      { id: 5, first_name: 'Lanna', last_name: 'Smead' },
+      { id: 6, first_name: 'Chev', last_name: 'Chark' },
+      { id: 7, first_name: 'Frazer', last_name: 'Haack' },
+      { id: 8, first_name: 'Jefferson', last_name: 'Kaemena' },
+      { id: 9, first_name: 'Sander', last_name: 'Lauridsen' },
+      { id: 10, first_name: 'Isadore', last_name: 'Berrey' },
+      { id: 11, first_name: 'Sol', last_name: 'Guillem' },
+    ],
+  },
+  {
+    teamName: 'England',
+    players: [
+      { id: 50, first_name: 'Dorice', last_name: 'Guys' },
+      { id: 51, first_name: 'Aggi', last_name: 'Sendley' },
+      { id: 52, first_name: 'Derek', last_name: 'Pretswell' },
+      { id: 53, first_name: 'Lenore', last_name: 'McNuff' },
+      { id: 54, first_name: 'Babita', last_name: 'Sydry' },
+      { id: 55, first_name: 'Germaine', last_name: 'Roscamp' },
+      { id: 56, first_name: 'Fey', last_name: 'Golagley' },
+      { id: 57, first_name: 'Alyda', last_name: 'Wilcockes' },
+      { id: 58, first_name: 'Maridel', last_name: 'Skittrall' },
+      { id: 59, first_name: 'Clarabelle', last_name: 'Coneron' },
+      { id: 60, first_name: 'Elvira', last_name: 'Glasson' },
+      { id: 61, first_name: 'Reine', last_name: 'Lesley' },
+    ],
+  },
+];
