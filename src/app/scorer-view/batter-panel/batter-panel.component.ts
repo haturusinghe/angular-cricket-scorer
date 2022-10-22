8d737a5f96283cb92dc8fb3efabc455041da456c
S.Haturusinghe <haturusingh3@gmail.com>
2022-10-22 13:23:03 +0530

    clean up

--- src/app/scorer-view/batter-panel/batter-panel.component.ts
+++ src/app/scorer-view/batter-panel/batter-panel.component.ts
@@ -8,9 +8,6 @@ import { MatchDataServiceService } from '../services/match-data-service.service'
   styleUrls: ['./batter-panel.component.scss'],
 })
 export class BatterPanelComponent implements OnInit {
-  // displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  displayedColumns: string[] = ['name', 'r', 'b', '_4s', '_6s', 'sr'];
-
   displayColumns: string[] = [
     'player',
     'runs',
@@ -36,8 +33,6 @@ export class BatterPanelComponent implements OnInit {
     fours: -1,
     strikeRate: -1,
   };
-
-  dataSource = BAT_DATA;
 
   batTableData = [this.striker, this.nonStriker];
 
@@ -66,17 +61,3 @@ export class BatterPanelComponent implements OnInit {
     console.log(this.nonStriker);
   }
 }
-
-export interface BatterTableRow {
-  name: string;
-  r: number;
-  b: number;
-  _4s: number;
-  _6s: number;
-  sr: number;
-}
-
-const BAT_DATA: BatterTableRow[] = [
-  { name: 'Jon Doe*', r: 4, b: 3, _4s: 1, _6s: 0, sr: 15.5 },
-  { name: 'Foo Bar', r: 20, b: 30, _4s: 1, _6s: 1, sr: 55.6 },
-];
