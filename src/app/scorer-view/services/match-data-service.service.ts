6b9bc92c5438d070955e50fa384251dc1a5edb3d
S.Haturusinghe <haturusingh3@gmail.com>
2022-10-22 12:27:16 +0530

    MatchDataService now provides match meta data

--- src/app/scorer-view/services/match-data-service.service.ts
+++ src/app/scorer-view/services/match-data-service.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@angular/core';
 import { Ball } from '../i/ball';
 import { Over } from '../i/over';
 import { Observable, of } from 'rxjs';
+import { PlayerDataService } from './player-data.service';
+import { Team } from '../i/team';
+import { OverData } from '../i/over-data';
 
 @Injectable({
   providedIn: 'root',
@@ -13,11 +16,31 @@ export class MatchDataServiceService {
 
   allOvers = new Array<Over>();
 
-  constructor() {}
+  tournamentName: string = 'England Tour of Sri Lanka';
+  teams: Team[] = this.playerDataService.getTeams();
+  battingTeamIndex: number = 1;
+  totalOvers: number = 50;
+
+  currentOver: OverData = { currentOver: 1, ballsLeft: 6 };
+
+  constructor(private playerDataService: PlayerDataService) {}
 
   getOverDetails(): Observable<Over[]> {
     const overs = of(this.allOvers);
     return overs;
+  }
+
+  getMatchMetaDetails() {
+    return {
+      tName: this.tournamentName,
+      totalOvers: this.totalOvers,
+      batting: this.teams[this.battingTeamIndex].teamName,
+    };
+  }
+
+  getCurrentOver(): Observable<OverData> {
+    const c = of(this.currentOver);
+    return c;
   }
 
   recordBall(ball: Ball) {
@@ -42,12 +65,15 @@ export class MatchDataServiceService {
       });
       console.log(this.allOvers);
       this.ballLeftForOver = 6;
+      // this.currentOver.ballsLeft = 6;
       this.currentOverNumber++;
+
       this.ballsForThisOver = new Array<Ball>();
     }
 
     if (!(ball.extras.includes('WD') || ball.extras.includes('NB'))) {
       this.ballLeftForOver--;
+      this.currentOver.ballsLeft--;
     }
 
     console.log(
@@ -59,5 +85,10 @@ export class MatchDataServiceService {
 
     this.ballsForThisOver.push(ball);
     console.log(this.ballsForThisOver);
+
+    if (this.ballLeftForOver == 0) {
+      this.currentOver.currentOver++;
+      this.currentOver.ballsLeft = 6;
+    }
   }
 }
