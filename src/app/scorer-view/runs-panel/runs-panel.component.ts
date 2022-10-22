5df56f7f0d72c7208ac28c030b014af792c5af43
S.Haturusinghe <haturusingh3@gmail.com>
2022-10-22 09:03:25 +0530

    fixed extras checkbox form functionality

--- src/app/scorer-view/runs-panel/runs-panel.component.ts
+++ src/app/scorer-view/runs-panel/runs-panel.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl } from '@angular/forms';
+import { MatchDataServiceService } from '../services/match-data-service.service';
 @Component({
   selector: 'crx-runs-panel',
   templateUrl: './runs-panel.component.html',
@@ -8,15 +8,21 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class RunsPanelComponent implements OnInit {
   run: number = 0;
-  extras = this._formBuilder.group({
-    WD: false,
-    NB: false,
-    B: false,
-    LB: false,
-  });
   isWicket: boolean = false;
 
-  wicket: Wicket[] = [
+  extrasToSend: Set<string> = new Set();
+
+  extraVals: Extra[] = [
+    { name: 'Wide', value: 'WD', isOn: false },
+    { name: 'No Ball', value: 'NB', isOn: false },
+  ];
+
+  extraValsBys: Extra[] = [
+    { name: 'Bye', value: 'By', isOn: false },
+    { name: 'Leg Bye', value: 'LBy', isOn: false },
+  ];
+
+  wicketVals: Wicket[] = [
     { value: 'B', viewValue: 'Bowled' },
     { value: 'C', viewValue: 'Caught' },
     { value: 'S', viewValue: 'Stumped' },
@@ -25,9 +31,9 @@ export class RunsPanelComponent implements OnInit {
     { value: 'HW', viewValue: 'Hit-wicket' },
   ];
 
-  wicketControl = new FormControl(this.wicket[0].value);
+  wicketControl = new FormControl(this.wicketVals[0].value);
 
-  constructor(private _formBuilder: FormBuilder) {}
+  constructor(private matchDataService: MatchDataServiceService) {}
 
   ngOnInit(): void {}
 
@@ -36,19 +42,62 @@ export class RunsPanelComponent implements OnInit {
   }
 
   onClick() {
-    console.log(this.run);
-    console.log(this.extras.value);
-    if (this.isWicket) {
-      console.log(this.wicketControl.value);
-    }
+    this.matchDataService.recordBall({
+      runs: this.run,
+      is4: this.run == 4,
+      is6: this.run == 6,
+      extras: Array.from(this.extrasToSend),
+      Out: {
+        isOut: this.isWicket,
+        type: this.isWicket ? this.wicketControl.value?.toString() : '',
+      },
+    });
   }
 
   setWicketStatus() {
     this.isWicket = !this.isWicket;
+  }
+
+  updateExtraValues(val: Extra) {
+    if (val.value == 'WD' && val.isOn) {
+      this.extraVals[1].isOn = false;
+    }
+
+    if (val.value == 'NB' && val.isOn) {
+      this.extraVals[0].isOn = false;
+    }
+
+    if (val.value == 'By' && val.isOn) {
+      this.extraValsBys[1].isOn = false;
+    }
+
+    if (val.value == 'LBy' && val.isOn) {
+      this.extraValsBys[0].isOn = false;
+    }
+
+    this.extrasToSend.clear();
+    this.extraVals.forEach((e) => {
+      if (e.isOn) {
+        this.extrasToSend.add(e.value);
+      }
+    });
+    this.extraValsBys.forEach((e) => {
+      if (e.isOn) {
+        this.extrasToSend.add(e.value);
+      }
+    });
+
+    console.log(this.extrasToSend);
   }
 }
 
 interface Wicket {
   value: string;
   viewValue: string;
+}
+
+export interface Extra {
+  name: string;
+  value: string;
+  isOn: boolean;
 }
