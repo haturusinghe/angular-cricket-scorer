5ffa5af2344877b43fcfbfd02d01afeae4fc47aa
S.Haturusinghe <haturusingh3@gmail.com>
2022-10-24 11:35:11 +0530

    fixed display string for chips

--- src/app/scorer-view/by-ball/over-card/over-card.component.ts
+++ src/app/scorer-view/by-ball/over-card/over-card.component.ts
@@ -14,4 +14,46 @@ export class OverCardComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
+
+  getType(d: any) {
+    let match = '';
+    let extraVals = [
+      { name: 'Wide', value: 'WD', isOn: false },
+      { name: 'No Ball', value: 'NB', isOn: false },
+    ];
+
+    let extraValsBys = [
+      { name: 'Bye', value: 'By', isOn: false },
+      { name: 'Leg Bye', value: 'LBy', isOn: false },
+    ];
+
+    let wicketVals = [
+      { value: 'B', viewValue: 'Bowled' },
+      { value: 'C', viewValue: 'Caught' },
+      { value: 'S', viewValue: 'Stumped' },
+      { value: 'ROS', viewValue: 'Run-out Striker' },
+      { value: 'RON', viewValue: 'Run-out Non-striker' },
+      { value: 'HW', viewValue: 'Hit-wicket' },
+    ];
+
+    extraVals.forEach((v) => {
+      if (v.value == d) {
+        match = v.name;
+      }
+    });
+
+    extraValsBys.forEach((v) => {
+      if (v.value == d) {
+        match = v.name;
+      }
+    });
+
+    wicketVals.forEach((v) => {
+      if (v.value == d) {
+        match = v.viewValue;
+      }
+    });
+
+    return match;
+  }
 }
