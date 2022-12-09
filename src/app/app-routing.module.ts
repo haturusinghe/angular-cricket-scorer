869da1124b22da4cb838ef3ebb18e27c44a28b35
S.Haturusinghe <haturusi-cs18015@stu.kln.ac.lk>
2022-12-09 21:59:01 +0530

    added Authguard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './auth/auth.component';
 import { ScorerViewComponent } from './scorer-view/scorer-view.component';
+import { AuthGuard } from './shared/auth.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: AuthComponent },
-  { path: 'score', component: ScorerViewComponent },
+  { path: 'score', component: ScorerViewComponent, canActivate: [AuthGuard] },
 ];
 
 @NgModule({
