a96dc1882f24c5b19ddf5e63e9a725fc51a9eecd
S.Haturusinghe <haturusi-cs18015@stu.kln.ac.lk>
2022-12-09 21:34:41 +0530

    routes tryout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ScorerViewComponent } from './scorer-view/scorer-view.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'score', component: ScorerViewComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
