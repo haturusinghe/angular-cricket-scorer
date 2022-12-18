5f739410434714f09a41f98d7c19774c8c7567f6
S.Haturusinghe <haturusi-cs18015@stu.kln.ac.lk>
2022-12-18 23:17:06 +0530

    added date & time to toolbar

--- src/app/modules/dashboard/components/dash-home/dash-home.component.ts
+++ src/app/modules/dashboard/components/dash-home/dash-home.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthServiceService } from 'src/app/auth/service/auth-service.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription, timer } from 'rxjs';
+import { map, share } from 'rxjs/operators';
 
 @Component({
   selector: 'crx-dash-home',
@@ -8,6 +10,10 @@ import { AuthServiceService } from 'src/app/auth/service/auth-service.service';
   styleUrls: ['./dash-home.component.scss'],
 })
 export class DashHomeComponent implements OnInit {
+  time = new Date();
+  rxTime = new Date();
+  intervalId;
+
   links = [
     { name: 'Home', isActive: false, icon: 'home', route: 'dashboard' },
     {
@@ -30,11 +36,40 @@ export class DashHomeComponent implements OnInit {
     },
   ];
 
+  subscription: Subscription = new Subscription();
+
   showFiller = false;
-  constructor(private auth: AuthServiceService, private router: Router) {}
+  constructor(private auth: AuthServiceService, private router: Router) {
+    // Using Basic Interval
+    this.intervalId = setInterval(() => {
+      this.time = new Date();
+    }, 1000);
+  }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    // Using RxJS Timer
+    this.subscription = timer(0, 1000)
+      .pipe(
+        map(() => new Date()),
+        share()
+      )
+      .subscribe((time) => {
+        let hour = this.rxTime.getHours();
+        let minuts = this.rxTime.getMinutes();
+        let seconds = this.rxTime.getSeconds();
+        //let a = time.toLocaleString('en-US', { hour: 'numeric', hour12: true });
+        let NewTime = hour + ':' + minuts + ':' + seconds;
+        console.log(NewTime);
+        this.rxTime = time;
+      });
+  }
 
+  ngOnDestroy() {
+    clearInterval(this.intervalId);
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
   logout() {
     this.auth.signOut();
   }
