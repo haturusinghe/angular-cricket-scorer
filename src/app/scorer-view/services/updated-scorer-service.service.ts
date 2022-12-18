e4a385f248a9754abe6a4c446776cd4252649ef7
S.Haturusinghe <haturusi-cs18015@stu.kln.ac.lk>
2022-12-18 22:34:26 +0530

    recive , update, and send scorecard to API

--- src/app/scorer-view/services/updated-scorer-service.service.ts
+++ src/app/scorer-view/services/updated-scorer-service.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { PostScore } from '../i/post-scores';
+import { PostGameService } from './post-game.service';
+import { ScoreCard } from './score-card';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TestMatchScorerService {
-  constructor() {}
+  constructor(private postGameService: PostGameService) {}
   innings = {
     a_1: {},
     b_1: {},
@@ -14,7 +16,11 @@ export class TestMatchScorerService {
     b_2: {},
   };
 
-  updateInningData(scoreObj: PostScore[], inning: number) {
+  updateInningData(
+    scoreObj: PostScore[],
+    inning: number,
+    scorecard: ScoreCard
+  ) {
     if (inning == 1) {
       this.innings.a_1 = scoreObj[0];
       this.innings.b_1 = scoreObj[1];
@@ -22,10 +28,19 @@ export class TestMatchScorerService {
       this.innings.a_2 = scoreObj[0];
       this.innings.b_2 = scoreObj[1];
     }
+    scorecard.score_card['innings'] = this.innings;
+    console.log(scorecard);
+    this.sendScores(scorecard);
   }
 
   getInnings(): Observable<any> {
     const innings = of(this.innings);
     return innings;
+  }
+
+  sendScores(scoreCard: ScoreCard) {
+    this.postGameService.postScorecardToApi(scoreCard).subscribe((s) => {
+      console.log(s);
+    });
   }
 }
