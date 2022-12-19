c28217502e7cec6698d50fdc6ffc0607fda30a88
S.Haturusinghe <haturusi-cs18015@stu.kln.ac.lk>
2022-12-19 22:16:24 +0530

    change route

--- src/app/modules/dashboard/components/dash-res-test/dash-res-test.component.ts
+++ src/app/modules/dashboard/components/dash-res-test/dash-res-test.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { MatchDataServiceService } from 'src/app/scorer-view/services/match-data-service.service';
 import { PreGameDataService } from 'src/app/scorer-view/services/pre-game-data.service';
+import {
+  MatchSummary,
+  ResumeScoringService,
+} from 'src/app/scorer-view/services/resume-scoring.service';
 
 @Component({
   selector: 'crx-dash-res-test',
@@ -9,6 +13,7 @@ import { PreGameDataService } from 'src/app/scorer-view/services/pre-game-data.s
   styleUrls: ['./dash-res-test.component.scss'],
 })
 export class DashResTestComponent implements OnInit {
+  summaryList: MatchSummary[] = [];
   constructor(
     private preGameDataService: PreGameDataService,
     private matchService: MatchDataServiceService,
@@ -20,10 +25,10 @@ export class DashResTestComponent implements OnInit {
   resumeMatch() {
     this.preGameDataService.setStart(true);
     this.matchService.resumeScoringSession();
-    this.router.navigate(['dashboard']);
+    this.router.navigate(['dashboard/scorer']);
   }
 
   newMatch() {
-    this.router.navigate(['dashboard']);
+    this.router.navigate(['dashboard/scorer']);
   }
 }
