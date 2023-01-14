be0175bf5fa629a99ff06f21b2682c4d866d53b4
S.Haturusinghe <haturusi-cs18015@stu.kln.ac.lk>
2023-01-14 13:15:58 +0530

    added new component

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -6,9 +6,11 @@ import { DashHomeComponent } from './components/dash-home/dash-home.component';
 
 import { MaterialExampleModule } from '../../../material.module';
 import { DashResTestComponent } from './components/dash-res-test/dash-res-test.component';
+import { MyMatchesComponent } from './components/my-matches/my-matches.component';
+import { MatchCardComponent } from './components/my-matches/match-card/match-card.component';
 
 @NgModule({
-  declarations: [DashHomeComponent, DashResTestComponent],
+  declarations: [DashHomeComponent, DashResTestComponent, MyMatchesComponent, MatchCardComponent],
   imports: [CommonModule, MaterialExampleModule, DashboardRoutingModule],
 })
 export class DashboardModule {}
