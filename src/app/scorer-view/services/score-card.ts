39eef5400145365e9d7412dbf3025031977fc14e
S.Haturusinghe <haturusi-cs18015@stu.kln.ac.lk>
2023-01-16 09:05:04 +0530

    minor update

--- src/app/scorer-view/services/score-card.ts
+++ src/app/scorer-view/services/score-card.ts
@@ -2,7 +2,7 @@ export class ScoreCard {
   constructor(
     public match_id: string,
     public date: string,
-    public description: string,
+    public description: any,
     public team_one: string,
     public team_two: string,
     public score_card: any
