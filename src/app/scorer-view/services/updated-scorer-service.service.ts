5b04179acded6b4fd53b92e4d7f7d5ecca6357f2
NandunPerera <pererawn-cs18032@stu.kln.ac.lk>
2023-01-20 03:06:49 +0530

    fixing losing inning data when resuming

--- src/app/scorer-view/services/updated-scorer-service.service.ts
+++ src/app/scorer-view/services/updated-scorer-service.service.ts
@@ -1,3 +1,5 @@
+import { Scorecard } from './../i/score-card';
+import { TeamDataService } from './team-data.service';
 import { ResumeCard, CurrentPlayers } from './resume-scoring.service';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
@@ -13,7 +15,10 @@ import { BowlerScore } from '../i/bowler-score';
   providedIn: 'root',
 })
 export class TestMatchScorerService {
-  constructor(private postGameService: PostGameService) {}
+  constructor(
+    private postGameService: PostGameService,
+    private teamDataService: TeamDataService
+  ) {}
   innings: Inning = {
     a_1: {
       teamName: 'a',
@@ -77,8 +82,9 @@ export class TestMatchScorerService {
     notSwapped: boolean = false
     //after the end of the ending , did the teams switch ?
   ) {
+    this.getScores(resumeCard.score_card.match_id);
     console.log('Resume', resumeCard);
-    console.log(scoreObj);
+    console.log('svvvvvs', this.innings);
 
     const totalRuns = resumeCard.score_card.battingTeamScore.totalScore;
 
@@ -112,12 +118,10 @@ export class TestMatchScorerService {
         scoreObj[0].totalScore = totalRuns;
         scoreObj[0].extras = extras;
         this.innings.a_1 = scoreObj[0];
-        this.innings.b_1 = scoreObj[1];
-        console.log('hi');
+        this.innings.b_1.bowling = scoreObj[1].bowling;
+        this.innings.b_1.batting = scoreObj[1].batting;
+        this.innings.b_1.teamName = scoreObj[1].teamName;
       }
-
-      console.log('a_1', this.innings.a_1);
-      console.log('b_1', this.innings.b_1);
     } else if (inning == 2) {
       if (!notSwapped) {
         if (
@@ -133,7 +137,6 @@ export class TestMatchScorerService {
 
           this.innings.b_2 = scoreObj[1];
           this.innings.a_2.bowling = scoreObj[0].bowling;
-          console.log('hi from 2');
         } else if (
           scoreObj[0].teamName ==
           resumeCard.score_card.battingTeamScore.teamName
@@ -145,8 +148,10 @@ export class TestMatchScorerService {
           scoreObj[0].totalScore = totalRuns;
           scoreObj[0].extras = extras;
           this.innings.a_2 = scoreObj[0];
-          this.innings.b_2 = scoreObj[1];
-          console.log('hi');
+
+          this.innings.b_2.bowling = scoreObj[1].bowling;
+          this.innings.b_2.batting = scoreObj[1].batting;
+          this.innings.b_2.teamName = scoreObj[1].teamName;
         }
       } else {
         if (
@@ -162,7 +167,6 @@ export class TestMatchScorerService {
 
           this.innings.b_2 = scoreObj[0];
           this.innings.a_2.bowling = scoreObj[1].bowling;
-          console.log('hi from 2');
         } else if (
           scoreObj[1].teamName ==
           resumeCard.score_card.battingTeamScore.teamName
@@ -174,8 +178,10 @@ export class TestMatchScorerService {
           scoreObj[1].totalScore = totalRuns;
           scoreObj[1].extras = extras;
           this.innings.a_2 = scoreObj[1];
-          this.innings.b_2 = scoreObj[0];
-          console.log('hi');
+
+          this.innings.b_2.bowling = scoreObj[1].bowling;
+          this.innings.b_2.batting = scoreObj[1].batting;
+          this.innings.b_2.teamName = scoreObj[1].teamName;
         }
       }
     }
@@ -193,6 +199,22 @@ export class TestMatchScorerService {
     console.log('HELLO FROM UPDATED SCORER SERVICE');
     this.postGameService.postScorecardToApi(scoreCard).subscribe((s) => {
       // console.log(s);
+    });
+  }
+
+  getScores(matchId: string) {
+    this.teamDataService.getSingleMatchData(matchId).subscribe((s) => {
+      let scorecard = JSON.parse(s.scorecard);
+      console.log('Post card', s);
+      if (scorecard.innings.a_1) {
+        this.innings.a_1 = scorecard.innings.a_1;
+      } else if (s.scorecard.innings.b_1) {
+        this.innings.b_1 = scorecard.innings.b_1;
+      } else if (s.scorecard.innings.a_2) {
+        this.innings.a_2 = scorecard.innings.a_2;
+      } else if (s.scorecard.innings.b_2) {
+        this.innings.b_2 = scorecard.innings.b_2;
+      }
     });
   }
 }
